ea5ac4e2d09fc4be3aee8c797b1ec07e0afd602a
JOHN <jestamatakos@gmail.com>
2015-03-26 14:10:16 -0400

    Adding Files/ Creating Repository
    Adding the files for the Student Planner

---
+++ Controllers/ToDoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentPlanner.Models;
+
+namespace StudentPlanner.Controllers
+{
+    public class ToDoesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: ToDoes
+        public ActionResult Index()
+        {
+            var toDoes = db.ToDoes.Include(t => t.AspNetUser);
+            return View(toDoes.ToList());
+        }
+
+        // GET: ToDoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDoes.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.AspNetUserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: ToDoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Item,Description,AspNetUserId")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ToDoes.Add(toDo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AspNetUserId = new SelectList(db.AspNetUsers, "Id", "Email", toDo.AspNetUserId);
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDoes.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AspNetUserId = new SelectList(db.AspNetUsers, "Id", "Email", toDo.AspNetUserId);
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Item,Description,AspNetUserId")] ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toDo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AspNetUserId = new SelectList(db.AspNetUsers, "Id", "Email", toDo.AspNetUserId);
+            return View(toDo);
+        }
+
+        // GET: ToDoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ToDo toDo = db.ToDoes.Find(id);
+            if (toDo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toDo);
+        }
+
+        // POST: ToDoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDo toDo = db.ToDoes.Find(id);
+            db.ToDoes.Remove(toDo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
